7d9d73603ffa269ac9bd59122ca6b94c19b54229
Maikael Lubis <maikael090904@gmail.com>
2026-03-04 22:56:25 +0700

    Ini baru saja ditambahkan kode surveillance.py

--- surveillance.py
+++ surveillance.py
@@ -11,6 +11,7 @@ drone = tello.Tello()
 drone.connect()
 print(drone.get_battery())
 global img
+
 drone.streamon()
 
 def getKeyboardInput():
@@ -29,11 +30,18 @@ def getKeyboardInput():
     if kp.getKey("a"): yv = -speed
     if kp.getKey("d"): yv = speed
 
-    if kp.getKey("q"): yv = drone.land(); time.sleep(3)
-    if kp.getKey("e"): yv = drone.takeoff() # tombol e dikeyboard adalah langkah awal setelah drone terkonek
+    #if kp.getKey("q"): yv = drone.land(); time.sleep(3)
+    #if kp.getKey("e"): yv = drone.takeoff() # tombol e dikeyboard adalah langkah awal setelah drone terkonek
+
+    if kp.getKey("q"):
+        drone.land()
+        time.sleep(3)
+    
+    if kp.getKey("e"):
+        drone.takeoff()
 
     if kp.getKey('z'):
-        cv2.imwrite(f'Resources/Images/{time.time()}.jpg')
+        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
         time.sleep(0.3)
 
     
@@ -44,14 +52,23 @@ def getKeyboardInput():
 
 
 
-drone.takeoff()
+#drone.takeoff()
 
+
+frame_read = drone.get_frame_read()
 
 while True:
     values = getKeyboardInput()
     drone.send_rc_control(values[0], values[1], values[2], values[3])
-    img = drone.get_frame_read().frame
+
+    img = frame_read.frame
     img = cv2.resize(img, (360, 240))
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:  # ESC
+        drone.land()
+        break
+
     sleep(0.05)
